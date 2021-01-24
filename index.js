0f2e2e748d94a183a8b20e92eb7364debfe7ac81
Nunes <GilsonNunes_dev@outlook.com>
2021-01-24 12:37:40 -0300

    added index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", function (msg) {
+  const chatId = msg.chat.id;
+  console.log(msg.text);
+  bot.sendMessage(chatId, "Obrigado por sua mensagem");
+});
