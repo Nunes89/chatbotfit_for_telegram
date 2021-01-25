355e0547c58697623bf2ce0e0d356dd146ced010
Nunes <GilsonNunes_dev@outlook.com>
2021-01-25 10:58:01 -0300

    added integration betwween dialogflow and index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
+const dialogflow = require("./dialogflow");
 
 const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", function (msg) {
+bot.on("message", async function (msg) {
   const chatId = msg.chat.id;
   console.log(msg.text);
-  bot.sendMessage(chatId, "Obrigado por sua mensagem");
+
+  const dfResponse = await dialogflow.sendMessage(chatId.toString(), msg.text);
+
+  bot.sendMessage(chatId, dfResponse.text);
 });
